9318555f2b4de88626b11a5fb8dba0a0d0f6e040
Thomas Labrusse <labrusse.thomas@gmail.com>
2022-02-06 10:05:00 +0100

    comment: global error MW

--- server/app.js
+++ server/app.js
@@ -27,6 +27,7 @@ app.all('*', (req, res, next) => {
 	next(new AppError(`Can't find ${req.originalUrl} on this server`, 404))
 })
 
+// NOTE: Detected as an error handling MW because first parameter is "err"
 app.use(globalErrorHandler)
 
 export default app
